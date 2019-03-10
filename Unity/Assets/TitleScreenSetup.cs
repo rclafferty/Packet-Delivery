4a51490698ea5716b1dfa2a189e960d59008c498
Robert Lafferty <rclafferty@ksu.edu>
2019-03-10 09:38:08 -0500

    Scene Transition When Enter Door

---
+++ Unity/Assets/TitleScreenSetup.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TitleScreenSetup : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button start = GameObject.Find("StartButton").GetComponent<Button>();
+        start.onClick.AddListener(StartButtonAction);
+
+        Button quit = GameObject.Find("QuitButton").GetComponent<Button>();
+        quit.onClick.AddListener(QuitButtonAction);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void StartButtonAction()
+    {
+        GameObject.Find("LevelManager").GetComponent<LevelManager>().LoadLevel("town");
+    }
+
+    void QuitButtonAction()
+    {
+        GameObject.Find("LevelManager").GetComponent<LevelManager>().QuitGame();
+    }
+}
