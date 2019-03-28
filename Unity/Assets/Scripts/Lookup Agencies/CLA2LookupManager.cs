1669c9535408e57cbdf28b80fb30510202e8f895
Robert Lafferty <rclafferty@huskers.unl.edu>
2019-03-28 13:47:23 -0500

    Tried to rework CLA -- No longer works

---
+++ Unity/Assets/Scripts/Lookup Agencies/CLA2LookupManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CLA2LookupManager : MonoBehaviour
+{
+    [SerializeField]
+    LookupAgencyManager lookupManager;
+
+    List<Person> listOfPeople;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        listOfPeople = lookupManager.CLAListOfPeople;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public List<Person> People
+    {
+        get
+        {
+            return listOfPeople;
+        }
+    }
+}
