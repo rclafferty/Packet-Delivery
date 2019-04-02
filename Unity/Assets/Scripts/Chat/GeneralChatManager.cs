cf2d676d2a2a9e31ffeb3ee4713eaebebf38254f
Robert Lafferty <rclafferty@huskers.unl.edu>
2019-04-02 12:42:25 -0500

    Finished General Chat Manager redo -- Still need to test

--- Unity/Assets/Scripts/Chat/GeneralChatManager.cs
+++ Unity/Assets/Scripts/Chat/GeneralChatManager.cs
@@ -39,6 +39,9 @@ public class GeneralChatManager : MonoBehaviour
     string option1_message;
     string option2_message;
 
+    UnityAction option1Action;
+    UnityAction option2Action;
+
     readonly float CHAT_DELAY = 0.02f;
 
     // Start is called before the first frame update
@@ -97,6 +100,19 @@ public class GeneralChatManager : MonoBehaviour
         chatText_message = "How may we assist you?";
         option1_message = "I'm looking for someone.";
         option2_message = "I can't figure out where to go from here.";
+
+        option1Action = delegate { ShowInputField(true); };
+        option2Action = DepartTextAndButtons;
+    }
+
+    void DepartTextAndButtons()
+    {
+        chatText_message = "Come back soon!";
+        option1_message = "Bye.";
+        option2_message = "";
+
+        option1Action = GoToTown;
+        option2Action = null;
     }
 
     void ClearText()
@@ -118,15 +134,15 @@ public class GeneralChatManager : MonoBehaviour
             chatText_message = "Who are you looking for?";
             option1_message = "";
             option2_message = "This person.";
+
+            option1Action = null;
+            option2Action = LookupPerson;
         }
     }
 
     public void LookupPerson()
     {
         ShowInputField(false);
-
-        UnityAction a1 = null;
-        UnityAction a2 = null;
 
         if (inputField.text == gameplayManager.CurrentTarget)
         {
@@ -136,9 +152,9 @@ public class GeneralChatManager : MonoBehaviour
             option2_message = "Bye.";
 
             // a1 = delegate { ShowInputField(true); };
-            a1 = GoToTown;
+            option1Action = GoToTown;
             // a2 = StartTextAndButtons;
-            a2 = null;
+            option2Action = null;
         }
         else
         {
@@ -146,10 +162,10 @@ public class GeneralChatManager : MonoBehaviour
             option1_message = "Whoops.";
             option2_message = "I'll try again later";
 
-            a1 = delegate { ShowInputField(true); };
+            option1Action = delegate { ShowInputField(true); };
 
             // a2 = StartTextAndButtons;
-            a2 = null;
+            option2Action = null;
         }
     }
 
@@ -161,10 +177,71 @@ public class GeneralChatManager : MonoBehaviour
     public void DisplayText(string c, string o1, string o2, UnityAction a1, UnityAction a2, out bool isClickable)
     {
         isClickable = false;
+
+        WriteText(c, o1, o2);
+        AddEventListeners(a1, a2);
+
+        isClickable = true;
     }
 
     IEnumerator WriteText(string chat, string option1, string option2)
     {
-        return null;
+        ClearText();
+
+        // Write chat text
+        for (int i = 0; i < chat.Length; i++)
+        {
+            // Wait for CHAT_DELAY seconds before writing the next letter
+            yield return new WaitForSeconds(CHAT_DELAY);
+            chatText.text += chat[i];
+        }
+
+        // If there is no text for option 1
+        if (string.IsNullOrEmpty(option2))
+        {
+            // Disable the button
+            option1Button.gameObject.SetActive(false);
+        }
+        else
+        {
+            // Write option 1 text
+            for (int i = 0; i < option1.Length; i++)
+            {
+                // Wait for CHAT_DELAY seconds before writing the next letter
+                yield return new WaitForSeconds(CHAT_DELAY);
+                option1Text.text += option1[i];
+            }
+        }
+
+        // If there is no text for option 2
+        if (string.IsNullOrEmpty(option2))
+        {
+            // Disable the button
+            option2Button.gameObject.SetActive(false);
+        }
+        else
+        {
+            // Make sure the button is enabled
+            option2Button.gameObject.SetActive(true);
+
+            // Write option 2 text
+            for (int i = 0; i < option2.Length; i++)
+            {
+                // Wait for CHAT_DELAY seconds before writing the next letter
+                yield return new WaitForSeconds(CHAT_DELAY);
+                option2Text.text += option2[i];
+            }
+        }
+    }
+
+    void AddEventListeners(UnityAction a1, UnityAction a2)
+    {
+        // Add option 1 listener
+        option1Button.onClick.RemoveAllListeners();
+        option1Button.onClick.AddListener(a1);
+
+        // Add option 2 listener
+        option2Button.onClick.RemoveAllListeners();
+        option2Button.onClick.AddListener(a2);
     }
 }
