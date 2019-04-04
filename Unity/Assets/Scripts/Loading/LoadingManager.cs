942cb1127e06b366a9dd70cae98acac8b187cca3
Robert Lafferty <rclafferty@huskers.unl.edu>
2019-04-04 02:30:40 -0500

    Music, MusicManager, Letter Templates

--- Unity/Assets/Scripts/Loading/LoadingManager.cs
+++ Unity/Assets/Scripts/Loading/LoadingManager.cs
@@ -9,19 +9,25 @@ public class LoadingManager : MonoBehaviour
     GameplayManager gameplayManager;
     LetterManager letterManager;
     LevelManager levelManager;
+    MusicManager musicManager;
     PlayerController playerController;
 
     GameObject prefab_CheatManager;
     GameObject prefab_GameplayManager;
     GameObject prefab_LevelManager;
     GameObject prefab_LetterManager;
+    GameObject prefab_MusicManager;
     GameObject prefab_PlayerController;
 
     GameObject object_CheatManager;
     GameObject object_GameplayManager;
     GameObject object_LevelManager;
     GameObject object_LetterManager;
+    GameObject object_MusicManager;
     GameObject object_PlayerController;
+
+    [SerializeField]
+    AudioClip music;
 
     // Start is called before the first frame update
     void Start()
@@ -42,35 +48,44 @@ public class LoadingManager : MonoBehaviour
         prefab_GameplayManager = Resources.Load<GameObject>("Prefabs/GameplayManager");
         prefab_LetterManager = Resources.Load<GameObject>("Prefabs/LetterManager");
         prefab_LevelManager = Resources.Load<GameObject>("Prefabs/LevelManager");
-        prefab_PlayerController = Resources.Load<GameObject>("Prefabs/Player");
+        prefab_MusicManager = Resources.Load<GameObject>("Prefabs/MusicManager");
+        // prefab_PlayerController = Resources.Load<GameObject>("Prefabs/Player");
 
         // Instantiate prefabs
         object_CheatManager = Instantiate(prefab_CheatManager, Vector3.zero, Quaternion.identity);
         object_GameplayManager = Instantiate(prefab_GameplayManager, Vector3.zero, Quaternion.identity);
         object_LetterManager = Instantiate(prefab_LetterManager, Vector3.zero, Quaternion.identity);
         object_LevelManager = Instantiate(prefab_LevelManager, Vector3.zero, Quaternion.identity);
-        object_PlayerController = Instantiate(prefab_PlayerController, Vector3.zero, Quaternion.identity);
+        object_MusicManager = Instantiate(prefab_MusicManager, Vector3.zero, Quaternion.identity);
+        // object_PlayerController = Instantiate(prefab_PlayerController, Vector3.zero, Quaternion.identity);
 
         // Change names of objects for consistency
         object_CheatManager.name = "CheatManager";
         object_GameplayManager.name = "GameplayManager";
         object_LetterManager.name = "LetterManager";
         object_LevelManager.name = "LevelManager";
-        object_PlayerController.name = "Player";
+        object_MusicManager.name = "MusicManager";
+        // object_PlayerController.name = "Player";
 
         // Get the components
         cheatManager = object_CheatManager.GetComponent<CheatManager>();
         gameplayManager = object_GameplayManager.GetComponent<GameplayManager>();
         letterManager = object_LetterManager.GetComponent<LetterManager>();
         levelManager = object_LevelManager.GetComponent<LevelManager>();
-        playerController = object_PlayerController.GetComponent<PlayerController>();
+        musicManager = object_MusicManager.GetComponent<MusicManager>();
+        // playerController = object_PlayerController.GetComponent<PlayerController>();
+
+        // Load Audio Clip from file
+        // music = Resources.Load("Music/LaserGroove") as AudioClip;
 
         // Do something with them -- ORDER SENSITIVE
         letterManager.LoadLetterFromFile();
         gameplayManager.SetLetterManager(letterManager);
 
         // Do something with them (cont) -- NOT order sensitive
-        levelManager.SetPlayerController(playerController);
+        musicManager.SetAudioClip(music);
+        musicManager.Play();
+        // levelManager.SetPlayerController(playerController);
         cheatManager.SetLevelManager(levelManager);
 
         // DONE
---
+++ Unity/Assets/Scripts/Music/MusicManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    static MusicManager instance = null;
+
+    [SerializeField]
+    AudioSource musicSource;
+    AudioClip musicClip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        musicSource = GameObject.Find("MusicSource").GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetAudioClip(AudioClip ac)
+    {
+        // Save music clip
+        musicClip = ac;
+
+        // Set music clip in AudioSource
+        musicSource.clip = musicClip;
+
+        // Loop the clip
+        musicSource.loop = true;
+    }
+
+    public void Play()
+    {
+        musicSource.Play();
+    }
+
+    public void Pause()
+    {
+        musicSource.Pause();
+    }
+
+    public void Stop()
+    {
+        musicSource.Stop();
+    }
+
+    public void ResetMusic()
+    {
+        musicSource.Stop();
+        musicSource.Play();
+    }
+}
