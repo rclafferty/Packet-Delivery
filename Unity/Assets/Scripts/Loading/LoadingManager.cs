66b27d66f58a52aad23be7dcc1dbea3af476f966
Robert Lafferty <rclafferty@huskers.unl.edu>
2019-04-25 01:23:44 -0500

    Updated Loading Manager

--- Unity/Assets/Scripts/Loading/LoadingManager.cs
+++ Unity/Assets/Scripts/Loading/LoadingManager.cs
@@ -49,7 +49,11 @@ public class LoadingManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(LoadGame());
+        Debug.Log("Loading game...");
+        // LoadGame();
+        // StartCoroutine(LoadGame());
+        Debug_LoadGame(false);
+        Debug.Log("Done");
     }
 
     // Update is called once per frame
@@ -58,7 +62,92 @@ public class LoadingManager : MonoBehaviour
         
     }
 
-    IEnumerator LoadGame()
+    void Debug_LoadGame(bool tf)
+    {
+        if (tf)
+        {
+            LoadGame_Method();
+        }
+        else
+        {
+            StartCoroutine(LoadGame_Coroutine());
+        }
+    }
+
+    void LoadGame_Method()
+    {
+        // Load prefabs
+        prefab_CheatManager = Resources.Load<GameObject>("Prefabs/CheatManager");
+        prefab_GameplayManager = Resources.Load<GameObject>("Prefabs/GameplayManager");
+        prefab_LetterManager = Resources.Load<GameObject>("Prefabs/LetterManager");
+        prefab_LevelManager = Resources.Load<GameObject>("Prefabs/LevelManager");
+        prefab_MusicManager = Resources.Load<GameObject>("Prefabs/MusicManager");
+        prefab_SpawnManager = Resources.Load<GameObject>("Prefabs/SpawnManager");
+        prefab_StartSceneLoader = Resources.Load<GameObject>("Prefabs/StartSceneLoader");
+        prefab_Timer = Resources.Load<GameObject>("Prefabs/Timer");
+        prefab_UpgradeManager = Resources.Load<GameObject>("Prefabs/UpgradeManager");
+
+        // Instantiate prefabs
+        object_CheatManager = Instantiate(prefab_CheatManager, Vector3.zero, Quaternion.identity);
+        object_GameplayManager = Instantiate(prefab_GameplayManager, Vector3.zero, Quaternion.identity);
+        object_LetterManager = Instantiate(prefab_LetterManager, Vector3.zero, Quaternion.identity);
+        object_LevelManager = Instantiate(prefab_LevelManager, Vector3.zero, Quaternion.identity);
+        object_MusicManager = Instantiate(prefab_MusicManager, Vector3.zero, Quaternion.identity);
+        object_SpawnManager = Instantiate(prefab_SpawnManager, Vector3.zero, Quaternion.identity);
+        object_StartSceneLoader = Instantiate(prefab_StartSceneLoader, Vector3.zero, Quaternion.identity);
+        object_Timer = Instantiate(prefab_Timer, Vector3.zero, Quaternion.identity);
+        object_UpgradeManager = Instantiate(prefab_UpgradeManager, Vector3.zero, Quaternion.identity);
+
+        // Change names of objects for consistency
+        object_CheatManager.name = "CheatManager";
+        object_GameplayManager.name = "GameplayManager";
+        object_LetterManager.name = "LetterManager";
+        object_LevelManager.name = "LevelManager";
+        object_MusicManager.name = "MusicManager";
+        object_SpawnManager.name = "SpawnManager";
+        object_StartSceneLoader.name = "StartSceneLoader";
+        object_Timer.name = "Timer";
+        object_UpgradeManager.name = "UpgradeManager";
+
+        // Get the components
+        cheatManager = object_CheatManager.GetComponent<CheatManager>();
+        gameplayManager = object_GameplayManager.GetComponent<GameplayManager>();
+        letterManager = object_LetterManager.GetComponent<LetterManager>();
+        levelManager = object_LevelManager.GetComponent<LevelManager>();
+        musicManager = object_MusicManager.GetComponent<MusicManager>();
+        spawnManager = object_SpawnManager.GetComponent<RespawnManager>();
+        startSceneLoader = object_StartSceneLoader.GetComponent<StartSceneLoader>();
+        timer = object_Timer.GetComponent<Timer>();
+        upgradeManager = object_UpgradeManager.GetComponent<UpgradeManager>();
+
+        // Load Audio Clip from file
+        // music = Resources.Load("Music/LaserGroove") as AudioClip;
+
+        // Do something with them -- ORDER SENSITIVE
+        musicManager.SetAudioClip(music);
+        musicManager.Play();
+        letterManager.LoadLetterFromFile();
+        gameplayManager.SetTimer(timer);
+        gameplayManager.CompleteTask();
+        gameplayManager.SetLetterManager(letterManager);
+        gameplayManager.CurrentTargetMessage = letterManager.GetStartingMessage();
+        gameplayManager.SetUpgradeManager(upgradeManager);
+
+        // Do something with them (cont) -- NOT order sensitive
+        gameplayManager.CurrentSpawnLocation = spawnManager.GetSpawnPointByName("Office");
+        cheatManager.SetLevelManager(levelManager);
+        timer.SetDifficulty("Default");
+        startSceneLoader.sprite_PlayerDown = sprite_PlayerDown;
+        startSceneLoader.sprite_PlayerRight = sprite_PlayerRight;
+        startSceneLoader.sprite_NPCLeft = sprite_NPCLeft;
+        SceneManager.sceneLoaded += startSceneLoader.OnSceneWasLoaded;
+
+        // DONE
+        // yield return new WaitForSeconds(2);
+        levelManager.LoadLevel("title");
+    }
+
+    IEnumerator LoadGame_Coroutine()
     {
         // Load prefabs
         prefab_CheatManager = Resources.Load<GameObject>("Prefabs/CheatManager");
