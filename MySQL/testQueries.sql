56c2474adf327f95c727e152222ad05831cd1f55
Robert Lafferty <rclafferty@huskers.unl.edu>
2020-05-08 21:51:21 -0500

    Restructure files -- no more MySQL, PHP, CoreCode folders

--- MySQL/testQueries.sql
+++
@@ -1,3 +0,0 @@
-SELECT u.username as 'Name', l.levelID as 'Level ID', e.title as 'Event Title', l.currentScore as 'Score', l.comment as 'Log Message', l.time as 'Log Time' FROM UserLogs l
-	JOIN Users u ON l.userID = u.id
-    JOIN Events e ON l.eventID = e.id;
