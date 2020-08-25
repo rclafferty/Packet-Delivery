3b24495839cff393b08c5c36de9d5fa0922ceaf9
Robert Lafferty <rclafferty@huskers.unl.edu>
2020-08-25 18:13:24 -0500

    Got the multi-page lesson working, need to implement diagram for ETM lesson page 3

--- Assets/Scripts/Office/OfficeComputerLessonManager.cs
+++ Assets/Scripts/Office/OfficeComputerLessonManager.cs
@@ -10,13 +10,17 @@ public class OfficeComputerLessonManager : MonoBehaviour
     [SerializeField] GameObject addressBookLesson;
     [SerializeField] GameObject addressBookLessonButton;
     [SerializeField] GameObject exitTheMatrixLesson;
+    [SerializeField] GameObject exitTheMatrixLessonPage2;
+    [SerializeField] GameObject exitTheMatrixLessonPage3;
     [SerializeField] GameObject exitTheMatrixLessonButton;
+    [SerializeField] GameObject exitTheMatrixNextLessonButton;
+    [SerializeField] GameObject exitTheMatrixPreviousLessonButton;
 
     static bool hasShownExitTheMatrixLesson = false;
     static bool hasShownAddressBookLesson = false;
     static bool firstAddressBookLesson = true;
 
-    enum Lesson { None, ExitTheMatrix, AddressBook };
+    enum Lesson { None, ExitTheMatrixP1, ExitTheMatrixP2, ExitTheMatrixP3, AddressBook };
     Lesson currentLesson;
 
     // Start is called before the first frame update
@@ -25,6 +29,8 @@ public class OfficeComputerLessonManager : MonoBehaviour
         ReturnToComputer();
 
         hudManager = GameObject.Find("HUD").GetComponent<HUDManager>();
+
+        currentLesson = Lesson.None;
     }
 
     public void ToggleUIObjects(bool isShown)
@@ -45,7 +51,12 @@ public class OfficeComputerLessonManager : MonoBehaviour
     {
         ToggleUIObjects(isShown: true);
         exitTheMatrixLesson.SetActive(false);
+        exitTheMatrixLessonPage2.SetActive(false);
+        exitTheMatrixLessonPage3.SetActive(false);
         addressBookLesson.SetActive(false);
+
+        exitTheMatrixNextLessonButton.SetActive(false);
+        exitTheMatrixPreviousLessonButton.SetActive(false);
 
         if (currentLesson == Lesson.AddressBook && firstAddressBookLesson)
         {
@@ -58,12 +69,114 @@ public class OfficeComputerLessonManager : MonoBehaviour
         currentLesson = Lesson.None;
     }
 
-    public void ShowExitTheMatrixLesson()
+    void ShowETM()
+    {
+        exitTheMatrixLesson.SetActive(true);
+    }
+
+    void ShowETMPage(int page)
+    {
+        switch (page)
+        {
+            case 1:
+                Debug.Log("Showing Page 1");
+                currentLesson = Lesson.ExitTheMatrixP1;
+
+                // Show first lesson
+                exitTheMatrixLesson.SetActive(true);
+
+                // Hide other lessons
+                exitTheMatrixLessonPage2.SetActive(false);
+                exitTheMatrixLessonPage3.SetActive(false);
+
+                // Show next button
+                exitTheMatrixNextLessonButton.SetActive(true);
+
+                // Hide previous button
+                exitTheMatrixPreviousLessonButton.SetActive(false);
+                break;
+            case 2:
+                Debug.Log("Showing Page 2");
+                currentLesson = Lesson.ExitTheMatrixP2;
+
+                // Show second lesson
+                exitTheMatrixLessonPage2.SetActive(true);
+
+                // Hide other lessons
+                exitTheMatrixLesson.SetActive(false);
+                exitTheMatrixLessonPage3.SetActive(false);
+
+                // Show next button and previous button
+                exitTheMatrixNextLessonButton.SetActive(true);
+                exitTheMatrixPreviousLessonButton.SetActive(true);
+                
+                break;
+            case 3:
+                Debug.Log("Showing Page 3");
+                currentLesson = Lesson.ExitTheMatrixP3;
+
+                // Show third lesson
+                exitTheMatrixLessonPage3.SetActive(true);
+
+                // Hide other lessons
+                exitTheMatrixLesson.SetActive(false);
+                exitTheMatrixLessonPage2.SetActive(false);
+
+                // Hide next button
+                exitTheMatrixNextLessonButton.SetActive(false);
+
+                // Show previous button
+                exitTheMatrixPreviousLessonButton.SetActive(true);
+                break;
+            default:
+                Debug.Log("Incorrect page -- " + page);
+                break;
+        }
+    }
+
+    public void ShowPreviousExitTheMatrixLesson()
     {
         ToggleUIObjects(isShown: false);
-        exitTheMatrixLesson.SetActive(true);
 
-        currentLesson = Lesson.ExitTheMatrix;
+        switch (currentLesson)
+        {
+            case Lesson.ExitTheMatrixP2:
+                ShowETMPage(page: 1);
+                break;
+
+            case Lesson.ExitTheMatrixP3:
+                ShowETMPage(page: 2);
+                break;
+
+            default:
+                Debug.Log("Incorrect lesson -- Lesson." + currentLesson);
+                break;
+        }
+    }
+
+    public void ShowNextExitTheMatrixLesson()
+    {
+        ToggleUIObjects(isShown: false);
+
+        Debug.Log(currentLesson);
+        switch (currentLesson)
+        {
+            case Lesson.None:
+                ShowETMPage(page: 1);
+                break;
+
+            case Lesson.ExitTheMatrixP1:
+                ShowETMPage(page: 2);
+                break;
+
+            case Lesson.ExitTheMatrixP2:
+                ShowETMPage(page: 3);
+                break;
+
+            default:
+                Debug.Log("Incorrect lesson -- Lesson." + currentLesson);
+                break;
+        }
     }
 
     public void ShowAddressBookLesson()
@@ -76,9 +189,10 @@ public class OfficeComputerLessonManager : MonoBehaviour
 
     public void PurchaseExitTheMatrix()
     {
+        Debug.Log(hasShownExitTheMatrixLesson);
         if (!hasShownExitTheMatrixLesson)
         {
-            ShowExitTheMatrixLesson();
+            ShowNextExitTheMatrixLesson();
             hasShownExitTheMatrixLesson = true;
         }
     }
